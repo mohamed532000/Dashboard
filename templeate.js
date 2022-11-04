d13d5282a1e8f18d955975a9728f5befb0bdcf26
Mohamed Ezat <111153378+mohamed532000@users.noreply.github.com>
2022-11-04 05:15:03 +0200

    Add files via upload

---
+++ templeate.js
@@ -0,0 +1,25 @@
+// let home = document.getElementById("home");
+// let about = document.getElementById("about");
+// let portfolio = document.getElementById("portfolio");
+// let services = document.getElementById("services");
+// let contact = document.getElementById("contact");
+
+
+// let allLinks = Array.from(document.querySelectorAll(".nav ul li"));
+
+// let allDivs = Array.from(document.querySelectorAll(".main-content .content-div"));
+
+// allLinks.forEach((link)=>{
+//     link.onclick = ()=>{
+
+
+
+//         allLinks.forEach((link)=>{
+//             link.classList.remove("active")
+//         })
+//         link.classList.add("active");
+//     }
+// })
+
+
+
